691fa7ba3fabe6b2aca026ac8e4751270bc00530
Jay Bhatt <jay.bhatt@razorpay.com>
2022-04-05 21:05:47 +0530

    feat: add rudderstack provider import in root module

--- src/app/root.module.ts
+++ src/app/root.module.ts
@@ -2,13 +2,21 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { UserTelemetryModule } from '@hypertrace/common';
+import { RudderStackTelemetry, UserTelemetryModule } from '@hypertrace/common';
 import { ObservabilityDashboardModule } from '@hypertrace/observability';
 import { ApplicationFrameModule } from './application-frame/application-frame.module';
 import { ConfigModule } from './config.module';
 import { RootComponent } from './root.component';
 import { RootRoutingModule } from './routes/root-routing.module';
 import { NavigationModule } from './shared/navigation/navigation.module';
+
+type CustomWindow = Window &
+  typeof globalThis & {
+    RUDDERSTACK_HT_DATAPLANE_URL?: string;
+    RUDDERSTACK_HT_WRITE_KEY?: string;
+  };
+
+declare const window: CustomWindow;
 
 @NgModule({
   imports: [
@@ -20,7 +28,18 @@ import { NavigationModule } from './shared/navigation/navigation.module';
     HttpClientModule,
     ApplicationFrameModule,
     ObservabilityDashboardModule,
-    UserTelemetryModule.forRoot([])
+    UserTelemetryModule.forRoot([
+      {
+        telemetryProvider: RudderStackTelemetry,
+        enableErrorTracking: true,
+        enableEventTracking: true,
+        enablePageTracking: true,
+        initConfig: {
+          orgId: window.RUDDERSTACK_HT_DATAPLANE_URL,
+          writeKey: window.RUDDERSTACK_HT_WRITE_KEY
+        }
+      }
+    ])
   ],
   declarations: [RootComponent],
   bootstrap: [RootComponent]

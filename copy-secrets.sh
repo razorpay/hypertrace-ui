3421a1c0b5ac60490e567185606e29ccd20d1818
Jay Bhatt <jay.bhatt@razorpay.com>
2022-04-05 16:04:44 +0530

    feat: added secret changes

---
+++ copy-secrets.sh
@@ -0,0 +1,3 @@
+touch secrets.js
+echo "window.RUDDERSTACK_HT_WRITE_KEY = \"$RUDDERSTACK_HT_WRITE_KEY\";" > secrets.js
+echo "window.RUDDERSTACK_HT_DATAPLANE_URL = \"$RUDDERSTACK_HT_DATAPLANE_URL\";" >> secrets.js
---
+++ src/secrets.js
@@ -0,0 +1,3 @@
+// Replace the below values with respective write key and dataplane URL on local
+window.RUDDERSTACK_HT_WRITE_KEY = 'RUDDERSTACK_HT_WRITE_KEY';
+window.RUDDERSTACK_HT_DATAPLANE_URL = 'RUDDERSTACK_HT_DATAPLANE_URL';

f27a0e6c12566e58fbd54bbf0a4d864bac0297c3
Akash Mane <akash.mane@razorpay.com>
2022-06-26 15:14:48 +0530

    fix: linting errors

--- src/app/shared/feature-resolver/feature-resolver.service.ts
+++ src/app/shared/feature-resolver/feature-resolver.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ApplicationFeature, FeatureState, FeatureStateResolver } from '@hypertrace/common';
+import { FeatureState, FeatureStateResolver } from '@hypertrace/common';
 import { Observable, of } from 'rxjs';
 import { UiConfigurationService } from './../ui-configuration/ui-configuration.service';
 

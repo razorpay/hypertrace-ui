bbaf0f6c736b059b1ee8db89c44083b774598830
Jay Bhatt <jay.bhatt@razorpay.com>
2022-08-03 13:21:09 +0530

    chore: analytics changes in dynamic config service

--- src/app/shared/dynamic-configuration/dynamic-configuration.service.ts
+++ src/app/shared/dynamic-configuration/dynamic-configuration.service.ts
@@ -1,5 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, Optional, SkipSelf } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import type { CustomWindow } from '../../root.module';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -15,10 +19,12 @@ export class DynamicConfigurationService {
   }
 
   public load(): Observable<UiConfiguration> {
-    return this.http.get<UiConfiguration>('/assets/json/config.json').pipe(tap((data: UiConfiguration) => {
-      this.config = data;
-      this.setupAnalyticsConfig();
-    }))
+    return this.http.get<UiConfiguration>('/assets/json/config.json').pipe(
+      tap((data: UiConfiguration) => {
+        this.config = data;
+        this.setupAnalyticsConfig();
+      })
+    );
   }
 
   public isConfigPresentForFeature(feature: string): boolean {
@@ -27,13 +33,21 @@ export class DynamicConfigurationService {
   public getValueForFeature(feature: string): string | number | boolean {
     return this.config?.featureFlags?.[feature];
   }
+  public setupAnalyticsConfig(): void {
+    (window as CustomWindow).analyticsConfig = this.config?.analyticsConfig;
+  }
 }
 
 interface UiConfiguration {
   featureFlags: FlagsConfig;
   urlConfig: FlagsConfig;
   dashboardConfig: FlagsConfig;
+  analyticsConfig: AnalyticsConfig;
 }
 interface FlagsConfig {
   [key: string]: string | boolean | number;
+}
+interface AnalyticsConfig {
+  enabled: boolean;
+  rudderstack_dataplane_url: string;
 }

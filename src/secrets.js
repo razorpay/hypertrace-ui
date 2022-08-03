0c4b904efdff97c441ad5bc5ab5c03e0eca437fa
Jay Bhatt <jay.bhatt@razorpay.com>
2022-08-03 14:06:47 +0530

    fix: update secrets.js file

--- src/secrets.js
+++ src/secrets.js
@@ -1,4 +1,4 @@
 // Replace the below values with respective write key and dataplane URL on local
 // Please be mindful while pushing changes in this file as it is served to the browser
 // Ensure that it doesn't have any secrets
-window.RUDDERSTACK_HT_WRITE_KEY = '2BpjcpXywwQujWTYRvcj5rpVwSb';
+window.RUDDERSTACK_HT_WRITE_KEY = 'RUDDERSTACK_HT_WRITE_KEY';

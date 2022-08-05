ea2c9722459d5283c6214b4511b4a909da48ae74
Jay Bhatt <jay.bhatt@razorpay.com>
2022-08-05 10:33:11 +0530

    fix: write the secret instead of appending

--- copy-secrets.sh
+++ copy-secrets.sh
@@ -1,3 +1,3 @@
 FILENAME="/usr/share/nginx/html/secrets.js"
 touch $FILENAME
-echo "window.RUDDERSTACK_HT_WRITE_KEY = \"$RUDDERSTACK_HT_WRITE_KEY\";" >> $FILENAME
+echo "window.RUDDERSTACK_HT_WRITE_KEY = \"$RUDDERSTACK_HT_WRITE_KEY\";" > $FILENAME

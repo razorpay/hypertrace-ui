f8ebcff587db45b30bfc20aeba7caa677a000b55
Abhijeet Singh <abhijeet.singh@razorpay.com>
2022-09-28 23:12:43 +0530

    test: service methods

--- projects/observability/src/pages/apis/service-detail/instrumentation/service-instrumentation.service.test.ts
+++ projects/observability/src/pages/apis/service-detail/instrumentation/service-instrumentation.service.test.ts
@@ -9,4 +9,14 @@ describe('ServiceInstrumentationService', () => {
   test('returns correct label for score', () => {
     expect(service.getLabelForScore(50)).toBe('Average');
   });
+
+  test('returns correct color for score', () => {
+    expect(service.getColorForScore(50).dark).toBe('#ffa01c');
+  });
+
+  test('returns correct description for score', () => {
+    expect(service.getDescriptionForScore(50)).toBe(
+      'There is considerable scope for improvement. Please see the sections below to learn how to improve the instrumentation of this service.'
+    );
+  });
 });

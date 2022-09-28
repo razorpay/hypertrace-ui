4b82b585ccca8f9310b0e783cb684a3a58171636
Abhijeet Singh <abhijeet.singh@razorpay.com>
2022-09-28 16:17:14 +0530

    test: getHeaderSummary

--- projects/observability/src/pages/apis/service-detail/instrumentation/components/instrumentation-details.component.test.ts
+++ projects/observability/src/pages/apis/service-detail/instrumentation/components/instrumentation-details.component.test.ts
@@ -3,6 +3,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { mockProvider } from '@ngneat/spectator/jest';
 
 import { ServiceInstrumentationService } from '../service-instrumentation.service';
+import { HeuristicScoreInfo } from '../service-instrumentation.types';
 import { InstrumentationDetailsComponent } from './instrumentation-details.component';
 
 describe('InstrumentationDetailsComponent', () => {
@@ -31,5 +32,11 @@ describe('InstrumentationDetailsComponent', () => {
 
   test('assigns correct color to icon', () => {
     expect(component.getIconColor(50)).toBe('#ffa01c');
+  });
+
+  test('shows correct header summary', () => {
+    expect(
+      component.getHeaderSummary({ sampleSize: '10', failureCount: '9', sampleType: 'span' } as HeuristicScoreInfo)
+    ).toBe('90% of spans failed this check');
   });
 });

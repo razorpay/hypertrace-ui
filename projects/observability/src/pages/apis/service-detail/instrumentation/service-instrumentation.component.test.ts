d9e6beab6a1cd9d0c7d4b15ef6c35bb893113b51
Abhijeet Singh <abhijeet.singh@razorpay.com>
2022-09-28 16:08:48 +0530

    test: getServiceScore

--- projects/observability/src/pages/apis/service-detail/instrumentation/service-instrumentation.component.test.ts
+++ projects/observability/src/pages/apis/service-detail/instrumentation/service-instrumentation.component.test.ts
@@ -1,7 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { mockProvider } from '@ngneat/spectator';
+import { BehaviorSubject } from 'rxjs';
 
 import { ServiceInstrumentationComponent } from './service-instrumentation.component';
+import { ServiceInstrumentationService } from './service-instrumentation.service';
 
 describe('ServiceInstrumentationComponent', () => {
   let component: ServiceInstrumentationComponent;
@@ -10,7 +13,12 @@ describe('ServiceInstrumentationComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ServiceInstrumentationComponent],
-      imports: [RouterTestingModule]
+      imports: [RouterTestingModule],
+      providers: [
+        mockProvider(ServiceInstrumentationService, {
+          serviceScoreSubject: new BehaviorSubject(undefined)
+        })
+      ]
     });
     fixture = TestBed.createComponent(ServiceInstrumentationComponent);
     component = fixture.componentInstance;
@@ -20,5 +28,9 @@ describe('ServiceInstrumentationComponent', () => {
 
   test('should be created successfully', () => {
     expect(component).toBeDefined();
+  });
+
+  test('uses subject from common service', () => {
+    expect(component.getServiceScore().getValue()).toBe(undefined);
   });
 });

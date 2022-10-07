9fcf91973c7796aa319e7044f3a98a71852169b9
Abhijeet Singh <abhijeet.singh@razorpay.com>
2022-10-07 10:58:18 +0530

    fix: tests

--- projects/observability/src/pages/apis/service-detail/instrumentation/components/instrumentation-details.component.test.ts
+++ projects/observability/src/pages/apis/service-detail/instrumentation/components/instrumentation-details.component.test.ts
@@ -62,6 +62,6 @@ describe('InstrumentationDetailsComponent', () => {
         sampleIds: [],
         score: -1.0
       })
-    ).toBe('Check skipped as no eligible spans in this run');
+    ).toBe('This check was skipped as no eligible spans were present in the last evaluation.');
   });
 });
--- projects/observability/src/pages/apis/service-detail/instrumentation/components/panel-content.component.test.ts
+++ projects/observability/src/pages/apis/service-detail/instrumentation/components/panel-content.component.test.ts
@@ -22,7 +22,7 @@ describe('PanelContentComponent', () => {
 
   test('navigates to Explorer page with trace ID filled', () => {
     expect(component.getExampleLink('1')).toBe(
-      '/explorer?time=1h&scope=endpoint-traces&series=column:count(calls)&filter=traceId_eq_1'
+      '/explorer?time=1d&scope=endpoint-traces&series=column:count(calls)&filter=traceId_eq_1'
     );
   });
 });

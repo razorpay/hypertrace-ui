36111ebd9661408ab9a1709c44ff06b9c7d55057
Jay Bhatt <jay.bhatt@razorpay.com>
2022-10-11 11:30:06 +0530

    feat: add more features to open in new tab component

--- projects/components/src/open-in-new-tab/open-in-new-tab.component.ts
+++ projects/components/src/open-in-new-tab/open-in-new-tab.component.ts
@@ -6,9 +6,11 @@ import { IconSize } from '../icon/icon-size';
 @Component({
   selector: 'ht-open-in-new-tab',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./open-in-new-tab.component.scss'],
   template: `
     <div *ngIf="this.paramsOrUrl" class="open-in-new-tab" htTooltip="Open in a new tab">
-      <ht-link [paramsOrUrl]="this.paramsOrUrl">
+      <ht-link [paramsOrUrl]="this.getParamsOrUrl()">
+        <span class="text" *ngIf="this.showLinkText">{{ this.getTextToDisplay() }}</span>
         <ht-icon icon="${IconType.OpenInNewTab}" [size]="this.iconSize"></ht-icon>
       </ht-link>
     </div>
@@ -20,4 +22,34 @@ export class OpenInNewTabComponent {
 
   @Input()
   public iconSize: IconSize = IconSize.Medium;
+
+  @Input()
+  public showLinkText: boolean = false;
+
+  @Input()
+  public linkPrefix: string = '';
+
+  public isNavigationParamsInstance(
+    params: ExternalNavigationParams | string | undefined
+  ): params is ExternalNavigationParams {
+    return typeof params !== 'string';
+  }
+  public getParamsOrUrl(): string | ExternalNavigationParams {
+    if (this.isNavigationParamsInstance(this.paramsOrUrl)) {
+      return {
+        ...this.paramsOrUrl,
+        url: `${this.linkPrefix}${this.paramsOrUrl?.url}`
+      };
+    }
+
+    return `${this.linkPrefix}${this.paramsOrUrl}`;
+  }
+
+  public getTextToDisplay(): string {
+    if (this.isNavigationParamsInstance(this.paramsOrUrl)) {
+      return this.paramsOrUrl.url;
+    }
+
+    return this.paramsOrUrl ?? '';
+  }
 }

50a2ccb5338fb43fcb47ec8a20e8d6aa9f6cee61
Jay Bhatt <jay.bhatt@razorpay.com>
2022-10-11 11:32:46 +0530

    feat: add a `open-in-new-tab` cell renderer

---
+++ projects/components/src/table/cells/data-renderers/open-in-new-tab/open-in-new-tab-cell-renderer.component.ts
@@ -0,0 +1,45 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ExternalNavigationParams, ExternalNavigationWindowHandling, NavigationParamsType } from '@hypertrace/common';
+import { IconSize } from 'projects/components/src/public-api';
+import { TableCellRenderer } from '../../table-cell-renderer';
+import { TableCellRendererBase } from '../../table-cell-renderer-base';
+import { CoreTableCellParserType } from '../../types/core-table-cell-parser-type';
+import { CoreTableCellRendererType } from '../../types/core-table-cell-renderer-type';
+import { TableCellAlignmentType } from '../../types/table-cell-alignment-type';
+
+@Component({
+  selector: 'ht-open-in-new-tab-table-cell-renderer',
+  styleUrls: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <ht-open-in-new-tab
+      [showLinkText]="this.getShowLinkText()"
+      [linkPrefix]="this.getLinkPrefix()"
+      iconSize="${IconSize.Small}"
+      [paramsOrUrl]="this.getParamsOrUrl()"
+    >
+    </ht-open-in-new-tab>
+  `
+})
+@TableCellRenderer({
+  type: CoreTableCellRendererType.OpenInNewTab,
+  alignment: TableCellAlignmentType.Left,
+  parser: CoreTableCellParserType.String
+})
+export class OpenInNewTabCellRendererComponent extends TableCellRendererBase<string> {
+  public getShowLinkText(): boolean {
+    return this?.rendererConfiguration?.showLinkText || false;
+  }
+
+  public getLinkPrefix(): string {
+    return this?.rendererConfiguration?.linkPrefix || '';
+  }
+
+  public getParamsOrUrl(): ExternalNavigationParams {
+    return {
+      navType: NavigationParamsType.External,
+      url: this.value,
+      windowHandling: ExternalNavigationWindowHandling.NewWindow
+    };
+  }
+}

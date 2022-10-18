185c431777a7e9e08ae2c5a89c907f3fa48bafc0
Jay Bhatt <jay.bhatt@razorpay.com>
2022-10-18 16:57:46 +0530

    feat: add type for sample ids to match response

--- projects/observability/src/pages/apis/service-detail/instrumentation/service-instrumentation.types.ts
+++ projects/observability/src/pages/apis/service-detail/instrumentation/service-instrumentation.types.ts
@@ -3,7 +3,7 @@ export interface HeuristicScoreInfo {
   description: string;
   evalTimestamp: string;
   score: number;
-  sampleIds: string[];
+  sampleIds: SampleHeuristicEnityId<SAMPLE_HEURISTIC_ENTITY_DELIMETER>[];
   sampleType: string;
   sampleSize: string;
   failureCount: string;
@@ -27,3 +27,7 @@ export interface OrgScoreResponse {
   heuristicClassScoreInfo: HeuristicClassScoreInfo[];
   aggregatedWeightedScore: number;
 }
+
+export type SampleHeuristicEnityId<Delimeter extends string> = `${string}${Delimeter}${string}`;
+
+export type SAMPLE_HEURISTIC_ENTITY_DELIMETER = ':' | ',';

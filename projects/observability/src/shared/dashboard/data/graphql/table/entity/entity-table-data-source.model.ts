6bfbc1cba6041cb80472238528d47fa712284480
Jay Bhatt <jay.bhatt@razorpay.com>
2022-12-13 03:08:50 +0530

    feat: add support for client side caching in entities table conditionally

--- projects/observability/src/shared/dashboard/data/graphql/table/entity/entity-table-data-source.model.ts
+++ projects/observability/src/shared/dashboard/data/graphql/table/entity/entity-table-data-source.model.ts
@@ -1,4 +1,11 @@
-import { TableDataRequest, TableDataResponse, TableRow } from '@hypertrace/components';
+import {
+  FilterOperator,
+  TableDataRequest,
+  TableDataResponse,
+  TableFilter,
+  TableRow,
+  TableSortDirection
+} from '@hypertrace/components';
 import {
   ARRAY_PROPERTY,
   Model,
@@ -9,9 +16,11 @@ import {
 } from '@hypertrace/hyperdash';
 import { EMPTY, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { isMetricAggregation, MetricAggregation } from '../../../../../graphql/model/metrics/metric-aggregation';
 import { Entity, EntityType } from '../../../../../graphql/model/schema/entity';
 import { GraphQlEntityFilter } from '../../../../../graphql/model/schema/filter/entity/graphql-entity-filter';
 import { GraphQlFilter } from '../../../../../graphql/model/schema/filter/graphql-filter';
+import { EntitySpecification } from '../../../../../graphql/model/schema/specifications/entity-specification';
 import { Specification } from '../../../../../graphql/model/schema/specifier/specification';
 import { EntitiesResponse } from '../../../../../graphql/request/handlers/entities/query/entities-graphql-query-builder.service';
 import {
@@ -59,17 +68,38 @@ export class EntityTableDataSourceModel extends TableDataSourceModel {
     filters: GraphQlFilter[],
     request: TableDataRequest<SpecificationBackedTableColumnDef>
   ): GraphQlEntitiesQueryRequest {
+    // tslint:disable-next-line: strict-boolean-expressions
+    if (this.isClientSideRendered) {
+      request.clientSideFilters = request.filters;
+      if (request.sort !== undefined) {
+        request.clientSideSort = {
+          direction: request.sort.direction,
+          column: request.sort.column
+        };
+      }
+    }
+    const offset = this.isClientSideRendered ? 0 : request.position.startIndex;
+
+    let sort = request.sort && {
+      direction: request.sort.direction,
+      key: request.sort.column.specification
+    };
+
+    if (this.isClientSideRendered && this.clientSideSort !== undefined) {
+      sort = {
+        direction: this.clientSideSort.direction,
+        key: request.columns[this.clientSideSort.defaultSortColumnIndex].specification
+      };
+    }
+
     return {
       requestType: ENTITIES_GQL_REQUEST,
       entityType: this.entityType,
       properties: request.columns.map(column => column.specification).concat(...this.additionalSpecifications),
-      limit: this.limit !== undefined ? this.limit : request.position.limit * 2, // Prefetch 2 pages
-      offset: request.position.startIndex,
-      sort: request.sort && {
-        direction: request.sort.direction,
-        key: request.sort.column.specification
-      },
-      filters: [...filters, ...this.toGraphQlFilters(request.filters)],
+      limit: this.limit ?? request.position.limit * 2, // Prefetch 2 pages,
+      offset: offset,
+      sort: sort,
+      filters: this.isClientSideRendered ? [] : [...filters, ...this.toGraphQlFilters(request.filters)],
       timeRange: this.getTimeRangeOrThrow(),
       includeTotal: true,
       includeInactive: request.includeInactive
@@ -80,10 +110,93 @@ export class EntityTableDataSourceModel extends TableDataSourceModel {
     response: EntitiesResponse,
     request: TableDataRequest<SpecificationBackedTableColumnDef>
   ): TableDataResponse<TableRow> {
+    let results: Entity<string>[] = response.results;
+    if (
+      this.isClientSideRendered &&
+      request.clientSideFilters?.length !== undefined &&
+      request.clientSideFilters.length > 0
+    ) {
+      request.clientSideFilters.forEach(clientSideFilter => {
+        const matchingColumn = this.findMatchingColumnConfigForClientSideFilter(
+          clientSideFilter.field,
+          request.columns
+        );
+        if (matchingColumn !== undefined) {
+          results = this.filterRespValuesForClientSideFilter(matchingColumn, results, clientSideFilter);
+        }
+      });
+    }
+
+    if (request.clientSideSort !== undefined && this.isClientSideRendered && results.length > 0) {
+      if (isMetricAggregation(results[0][request.clientSideSort.column.id])) {
+        results.sort((res1, res2) => {
+          if (request.clientSideSort!.direction === TableSortDirection.Ascending) {
+            return (
+              (res1[request.clientSideSort!.column.id] as MetricAggregation).value -
+              (res2[request.clientSideSort!.column.id] as MetricAggregation).value
+            );
+          }
+
+          return (
+            (res2[request.clientSideSort!.column.id] as MetricAggregation).value -
+            (res1[request.clientSideSort!.column.id] as MetricAggregation).value
+          );
+        });
+      } else {
+        results.sort((res1, res2) => {
+          if (request.clientSideSort!.direction === TableSortDirection.Ascending) {
+            return ((res1[request.clientSideSort!.column.id] as Entity).name as string).localeCompare(
+              (res2[request.clientSideSort!.column.id] as Entity).name as string
+            );
+          }
+
+          return ((res2[request.clientSideSort!.column.id] as Entity).name as string).localeCompare(
+            (res1[request.clientSideSort!.column.id] as Entity).name as string
+          );
+        });
+      }
+    }
+
     return {
-      data: this.resultsAsTreeRows(response.results, request, this.childEntityDataSource !== undefined),
-      totalCount: response.total!
+      data: this.resultsAsTreeRows(results, request, this.childEntityDataSource !== undefined),
+      totalCount: this.isClientSideRendered ? results.length : response.total ?? 0
     };
+  }
+
+  private findMatchingColumnConfigForClientSideFilter(
+    filterKeyName: string,
+    columnDefs: SpecificationBackedTableColumnDef[]
+  ): SpecificationBackedTableColumnDef | undefined {
+    return columnDefs.find(
+      maybeColumn =>
+        (maybeColumn.specification as EntitySpecification).asGraphQlOrderByFragment().expression.key === filterKeyName
+    );
+  }
+
+  private filterRespValuesForClientSideFilter(
+    columnDef: SpecificationBackedTableColumnDef,
+    rows: Entity[],
+    tableFilter: TableFilter
+  ): Entity<string>[] {
+    const actualKeyNameInRows = columnDef.specification.resultAlias();
+    // tslint:disable-next-line: prefer-switch
+    if (tableFilter.operator === FilterOperator.Equals) {
+      return rows.filter(eachRow => ((eachRow[actualKeyNameInRows] as Entity).name as string) === tableFilter.value);
+    }
+
+    if (tableFilter.operator === FilterOperator.Like) {
+      const regexExp = new RegExp(tableFilter.value as string);
+
+      return rows.filter(eachRow => regexExp.test((eachRow[actualKeyNameInRows] as Entity).name as string));
+    }
+
+    if (tableFilter.operator === FilterOperator.In) {
+      return rows.filter(eachRow =>
+        (tableFilter.value as string[]).includes((eachRow[actualKeyNameInRows] as Entity).name as string)
+      );
+    }
+
+    return rows;
   }
 
   private resultsAsTreeRows(

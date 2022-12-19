7893700a74737f9b554119154a4897e829de0a09
Abhijeet Singh <abhijeet.singh@razorpay.com>
2022-12-19 12:25:04 +0530

    feat: replace onReady with data

--- projects/observability/src/pages/explorer/explorer-dashboard-builder.ts
+++ projects/observability/src/pages/explorer/explorer-dashboard-builder.ts
@@ -10,19 +10,19 @@ import {
 import { Dashboard, ModelJson } from '@hypertrace/hyperdash';
 import { Observable, of, ReplaySubject, Subject } from 'rxjs';
 import { distinctUntilChanged, map, switchMap } from 'rxjs/operators';
+import { MetricAggregationType } from '../../public-api';
 import { ExploreVisualizationRequest } from '../../shared/components/explore-query-editor/explore-visualization-builder';
 import { LegendPosition } from '../../shared/components/legend/legend.component';
 import { ObservabilityTableCellType } from '../../shared/components/table/observability-table-cell-type';
 import { TracingTableCellType } from '../../shared/components/table/tracing-table-cell-type';
 import { ExplorerVisualizationCartesianDataSourceModel } from '../../shared/dashboard/data/graphql/explorer-visualization/explorer-visualization-cartesian-data-source.model';
-import { ExplorerVisualizationMetricDataSourceModel } from '../../shared/dashboard/data/graphql/explorer-visualization/explorer-visualization-metric-data-source.model';
 import { GraphQlFilterDataSourceModel } from '../../shared/dashboard/data/graphql/filter/graphql-filter-data-source.model';
 import {
   AttributeMetadata,
   AttributeMetadataType,
   toFilterAttributeType
 } from '../../shared/graphql/model/metadata/attribute-metadata';
-import { GraphQlFilter } from '../../shared/graphql/model/schema/filter/graphql-filter';
+import { GraphQlFilter, GraphQlOperatorType } from '../../shared/graphql/model/schema/filter/graphql-filter';
 import { ObservabilityTraceType } from '../../shared/graphql/model/schema/observability-traces';
 import { SPAN_SCOPE } from '../../shared/graphql/model/schema/span';
 import { MetadataService } from '../../shared/services/metadata/metadata.service';
@@ -68,32 +68,26 @@ export class ExplorerDashboardBuilder {
             type: 'metric-display-widget',
             title: `${seriesObject.specification.name} ${seriesObject.specification.aggregation}`,
             subscript: seriesObject.specification.name === 'duration' ? 'ms' : undefined,
-            /*
-             * TODO: Needs refactoring
-             * We shouldn't pass resultAlias directly to a model. A better approach is:
-             * -> Remove the 'metric-key' property from metric-display-widget.model
-             * -> Create a parent component 'metric-group-widget' that renders metric display widgets as children
-             * -> This parent uses the same data source as is being used now
-             * -> Parent receives array of 'titles' of widgets from seriesObject in the json passed here
-             * -> In the mapResponseData method of data source, extract the values from response using resultAlias from requestState
-             * -> Pass the array of these 'values' to the model of the parent
-             * -> Create parent's renderer that uses the 'titles' and 'values' arrays to render metric widgets
-             * -> Optional: Handle layout in the parent renderer so 'container-widget' can be removed
-             *
-             * Alternate approach:
-             * Try removing onReady and using data property instead to use 'metric-aggregation-data-source'
-             * Examples can be found in home.dashboard.ts
-             * If this works, ExplorerVisualizationMetricDataSourceModel can be deleted. Also, related "if" blocks in
-             * Metric Display Widget Model and ExploreCartesianDataSourceModel can be deleted.
-             */
-            'metric-key': seriesObject.specification.resultAlias()
+            data: {
+              type: 'metric-aggregation-data-source',
+              context: 'API_TRACE',
+              metric: {
+                type: 'explore-selection',
+                metric: 'calls',
+                aggregation: MetricAggregationType.Count
+              },
+              filters: [
+                {
+                  type: 'graphql-key-value-filter',
+                  key: 'duration',
+                  operator: GraphQlOperatorType.GreaterThan,
+                  value: 1000
+                }
+              ]
+            }
           }))
         },
-        onReady: dashboard => {
-          dashboard.createAndSetRootDataFromModelClass(ExplorerVisualizationMetricDataSourceModel);
-          const dataSource = dashboard.getRootDataSource<ExplorerVisualizationMetricDataSourceModel>()!;
-          dataSource.request = request;
-        }
+        onReady: () => undefined
       });
     }
 

9d9d6f370987cafc7f7d9a3a22871cd5512c5460
Abhijeet Singh <abhijeet.singh@razorpay.com>
2022-12-26 10:57:06 +0530

    docs: add comment

--- projects/observability/src/shared/components/explore-query-editor/explore-visualization-builder.ts
+++ projects/observability/src/shared/components/explore-query-editor/explore-visualization-builder.ts
@@ -212,7 +212,7 @@ export class ExploreVisualizationBuilder implements OnDestroy {
     // Todo: Revisit default value
     return {
       context: context,
-      interval: undefined,
+      interval: undefined, // setting this to 'AUTO' will also affect edit panel of custom dashboards
       resultLimit: ExploreVisualizationBuilder.DEFAULT_LIMIT,
       series: [this.buildDefaultSeries(context)]
     };
